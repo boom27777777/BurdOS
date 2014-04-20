07d3b9a0199e8b3572a993f781f43c753ef2223f
gravypod <gravypod@gravypod.com>
2014-04-20 09:22:02 -0400

    Save me those bits!

--- src/main.c
+++ src/main.c
@@ -2,7 +2,7 @@
  *  main.c The C kernel entry point for BurdOS!
  */
 
-int main(struct multiboot *mboot_ptr) {
+void main(struct multiboot *mboot_ptr) {
     #include "common.h"
     #include "monitor.h"
     const char src[50] = "Hello World! Big brother is watching!";
@@ -11,5 +11,4 @@ int main(struct multiboot *mboot_ptr) {
     memset(dest, 'F', strlen(dest));
     monitor_clear();
     monitor_write(dest);  
-    return 0;
 }
